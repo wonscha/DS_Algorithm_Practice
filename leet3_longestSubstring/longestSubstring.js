cfd840c456f3d1de376b039ad182805e939de687
Wonsuk Cha <wonsukcha@Wonsuks-Mac-mini.local>
2022-01-29 17:24:03 -0500

    add comments on leet3_longestSubstring solution

--- leet3_longestSubstring/longestSubstring.js
+++ leet3_longestSubstring/longestSubstring.js
@@ -10,19 +10,25 @@
 // s = "pwwkew" => 3
 
 const longestSubstring = (s) => {
+  // create variables to track maximum length and current substring
   let maxLength = 0;
   let subStr = '';
 
   for (let char of s) {
     if (!subStr.includes(char)) {
+      // add current character to subStr if it's not yet included
       subStr += char;
     } else {
+      // if a new character is already included in substring,
+      // remove the same character just came across and the part before that from substring. 
+      // Then add the new character.
       maxLength = Math.max(maxLength, subStr.length);
       subStr = subStr.substring(subStr.indexOf(char) + 1) + char;
     }
   }
 
-  return maxLength >= subStr.length ? maxLength : subStr.length;
+  // return max length
+  return Math.max(maxLength, subStr.length);
 }
 
 module.exports = longestSubstring;
